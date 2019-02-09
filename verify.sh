d083e4d665dc57c02f543b00ce1cfbc9a7c714ac
Markus Enzenberger <enz@users.sourceforge.net>
2019-02-09 20:06:01 +0100

    Simplified verify script

--- verify.sh
+++ verify.sh
@@ -2,36 +2,18 @@
 
 # Link from .git/hooks/pre-commit
 
-if grep -q $'\r' words; then
-  echo "words contains non-Unix line endings"
+function fatal {
+  echo $1
   exit 1
-fi
-if grep -q $'\r' blacklist; then
-  echo "blacklist contains non-Unix line endings"
-  exit 1
-fi
-if grep -q '^$' words; then
-  echo "words contains empty lines"
-  exit 1
-fi
-if grep -q '^$' blacklist; then
-  echo "blacklist contains empty lines"
-  exit 1
-fi
-if ! sort -cu words; then
-  echo "words not sorted"
-  exit 1
-fi
-if ! sort -cu blacklist; then
-  echo "blacklist not sorted"
-  exit 1
-fi
-if [ ! -z "`comm -12 blacklist words`" ]; then
-  echo "words and blacklist intersection not empty"
-  exit 1
-fi
-if grep -vi '^[abcdefghijklmnopqrstuvwxyzäöüßàáâåçčéèêēëīíïîłñōóõœšūûú]*$' words; then
-  echo "invalid characters in word list"
-  exit 1
-fi
+}
+
+! grep -q $'\r' words || fatal "words contains non-Unix line endings"
+! grep -q $'\r' blacklist || fatal "blacklist contains non-Unix line endings"
+! grep -q '^$' words || fatal "words contains empty lines"
+! grep -q '^$' blacklist || fatal "blacklist contains empty lines"
+sort -cu words || fatal "words not sorted"
+sort -cu blacklist || fatal "blacklist not sorted"
+test -z "`comm -12 blacklist words`" || fatal "words and blacklist overlap"
+! grep -vi '^[abcdefghijklmnopqrstuvwxyzäöüßàáâåçčéèêēëīíïîłñōóõœšūûú]*$' words \
+  || fatal "invalid characters in word list"
 
