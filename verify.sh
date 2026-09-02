9c4100a52016e391912e66761e83da36a91d854f
Markus Enzenberger <enz@users.sourceforge.net>
2026-09-02 11:20:06 +0200

    Simplified regex

--- verify.sh
+++ verify.sh
@@ -6,6 +6,7 @@ function fatal {
   echo $1
   exit 1
 }
+
 [[ "$(locale charmap)" == "UTF-8" ]] || fatal "script requires a UTF-8 locale"
 ! grep -q $'\r' words || fatal "words contains non-Unix line endings"
 ! grep -q $'\r' blacklist || fatal "blacklist contains non-Unix line endings"
@@ -14,6 +15,4 @@ function fatal {
 sort -cu words || fatal "words not sorted"
 sort -cu blacklist || fatal "blacklist not sorted"
 test -z "`comm -12 blacklist words`" || fatal "words and blacklist overlap"
-! grep -vi '^[abcdefghijklmnopqrstuvwxyzäöüßàáâåçčéèêēëīíïîłñōóõœšūûú]*$' words \
-  || fatal "invalid characters in word list"
-
+! grep -vi '^[a-zäöüßàáâåçčéèêēëīíïîłñōóõœšūûú]*$' words || fatal "invalid characters in word list"
